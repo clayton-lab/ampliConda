45e4edcee663fbf1b1a25f90794fe44b8981b6ad
TheSchinz <130110523+TheSchinz@users.noreply.github.com>
2026-06-10 04:14:29 -0500

    Update r_absolute_barplot.R

--- workflow/scripts/r_absolute_barplot.R
+++ workflow/scripts/r_absolute_barplot.R
@@ -9,6 +9,6 @@ library(tidyverse)
 
 barplot_absolute <- plot_bar(phyloseq, x="Sample", y="Abundance", fill=taxalevel) +
 geom_bar(aes(fill = .data[[taxalevel]], color = .data[[taxalevel]]), stat = "identity", position = "stack") +
-facet_wrap(reformulate(taxacomparison), scales = "free_x") + theme_bw()
+facet_wrap(reformulate(taxacomparison), scales = "free_x") + theme_bw() + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 
 ggsave(filename = snakemake@output[[1]], plot = barplot_absolute, device = "pdf")
