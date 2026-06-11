9eaa78ca0586369f623d078969c91e76cf0eb400
TheSchinz <130110523+TheSchinz@users.noreply.github.com>
2026-06-11 16:22:59 -0500

    Update r_heatmap.R

--- workflow/scripts/r_heatmap.R
+++ workflow/scripts/r_heatmap.R
@@ -1,7 +1,7 @@
 phyloseq = readRDS(snakemake@input$phyloseq)
 taxalevel = snakemake@params$taxalevel
 taxacomparison = snakemake@params$taxacomparison
-samplingdepth = snakemake@params$samplingdepth
+samplingdepth = as.integer(snakemake@params$samplingdepth)
 
 library(phyloseq)
 library(ggplot2)
@@ -9,14 +9,13 @@ library(tidyverse)
 library(dplyr)
 
 ps_ordered <- tax_glom(phyloseq, taxalevel)
-ps_ordered_rare <- rarefy_even_depth(ps_ordered, sample.size = samplingdepth, rngseed = 123, replace = FALSE)
 
-heatmap <- plot_heatmap(ps_ordered_rare, method = NULL, taxa.label = taxalevel, raster = FALSE)
+heatmap <- plot_heatmap(ps_ordered, method = NULL, taxa.label = taxalevel, raster = FALSE)
 
 real_heatmap <- heatmap + 
   ggplot2::theme_minimal() +
   ggplot2::theme(panel.grid = ggplot2::element_line(color = "grey80")) +
   ggplot2::geom_tile(color = "black", linewidth = 0.2) +
-  ggplot2::facet_wrap(reformulate(taxacomparison), scales = "free_x")
+  ggplot2::facet_wrap(reformulate(taxacomparison), scales = "free_x") + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 
 ggsave(filename = snakemake@output[[1]], plot = real_heatmap, device = "pdf")
